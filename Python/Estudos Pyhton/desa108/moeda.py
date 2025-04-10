2c80afff55ac9a44f40908166e2d0cc7b57640b3
BrunoHenri1 <olivierabruno16@gmail.com>
2025-04-10 16:19:43 -0300

    Estudos

--- Python/Estudos Pyhton/desa108/moeda.py
+++ Python/Estudos Pyhton/desa108/moeda.py
@@ -2,17 +2,21 @@ def aumentar(preço=0, taxa=0):
     porcent = (preço / 100) * taxa + preço
     return porcent
 
+
 def diminuir(preço=0, taxa=0):
     diminui = preço - (preço / 100) * taxa
     return diminui
+
 
 def dobro(preço=0):
     double = preço * 2
     return double
 
+
 def metade(preço=0):
     meio = preço / 2
     return meio
 
+
 def moeda(preço=0, moeda="R$"):
-    return f"{moeda}{preço}".replace('.',',')
+    return f"{moeda}{preço:>.2f}".replace('.',',')
--- Python/Estudos Pyhton/desa108/teste.py
+++ Python/Estudos Pyhton/desa108/teste.py
@@ -3,7 +3,7 @@ from moeda import *
 
 valor = float(input("Digite o valor: R$ "))
 
-print(f"A metade de R$ {moeda.valor} é R$ {metade(valor)}")
-print(f"O Dobro de R$ {moeda.valor} é R$ {dobro(valor)}")
-print(f"O valor R$ {moeda.valor} com 10% de desconto é R$ {diminuir(valor, 10)}")
-print(f"O valor R$ {moeda.valor} com 10% de acréscimo é R$ {aumentar(valor, 10)}")
+print(f"A metade de R$ {moeda(valor)} é R$ {moeda(metade(valor))}")
+print(f"O Dobro de R$ {moeda(valor)} é R$ {moeda(dobro(valor))}")
+print(f"O valor R$ {moeda(valor)} com 10% de desconto é R$ {moeda(diminuir(valor, 10))}")
+print(f"O valor R$ {moeda(valor)} com 10% de acréscimo é R$ {moeda(aumentar(valor, 10))}")
